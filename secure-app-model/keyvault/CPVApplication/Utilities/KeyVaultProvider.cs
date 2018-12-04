bfac2fb2dbfe516f6ded1f4a57bdd216ec551fd9
Isaiah Williams <isaiah.williams@microsoft.com>
2018-12-03 19:16:35 -0600

    Updated the NuGet packages for the CPV sample. Also, refactored the Key Vault provider to not create new instance of the HttpClient for each time a secret is requested.

--- secure-app-model/keyvault/CPVApplication/Utilities/KeyVaultProvider.cs
+++ secure-app-model/keyvault/CPVApplication/Utilities/KeyVaultProvider.cs
@@ -7,31 +7,64 @@
 namespace CPVApplication.Utilities
 {
     using System.Configuration;
+    using System.Net.Http;
     using System.Threading.Tasks;
     using Microsoft.Azure.KeyVault;
     using Microsoft.Azure.KeyVault.Models;
     using Newtonsoft.Json.Linq;
-
 
     /// <summary>
     /// Provider for accessing secrets from the Azure KeyVault
     /// </summary>
     public class KeyVaultProvider
     {
-        private readonly string KeyVaultClientId = ConfigurationManager.AppSettings["ida:KeyVaultClientId"];
-        private readonly string KeyVaultClientSecret = ConfigurationManager.AppSettings["ida:KeyVaultClientSecret"];
+        /// <summary>
+        /// The client used to perform HTTP operations. 
+        /// </summary>
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        /// <summary>
+        /// The base address for the instance of Azure Key Vault.
+        /// </summary>
         private readonly string BaseUrl = ConfigurationManager.AppSettings["KeyVaultEndpoint"];
 
+        /// <summary>
+        /// The identifier for the Azure AD application configured to access the instance of Azure Key Vault.
+        /// </summary>
+        private readonly string KeyVaultClientId = ConfigurationManager.AppSettings["ida:KeyVaultClientId"];
+
+        /// <summary>
+        /// The application secret for the Azure AD application configured to access the instance of Azure Key Vault.
+        /// </summary>
+        private readonly string KeyVaultClientSecret = ConfigurationManager.AppSettings["ida:KeyVaultClientSecret"];
+
+        /// <summary>
+        /// The client used to interact with the instance of Azure Key Vault.
+        /// </summary>
+        private readonly KeyVaultClient keyVaultClient;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KeyVaultProvider" /> class.
+        /// </summary>
+        public KeyVaultProvider()
+        {
+            keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(GetToken), httpClient);
+        }
+
+        /// <summary>
+        /// Gets the specified entity from the vault. 
+        /// </summary>
+        /// <param name="key">Identifier of the entity to be retrieved.</param>
+        /// <returns>The value retrieved from the vault.</returns>
         public async Task<string> GetSecretAsync(string key)
         {
-            KeyVaultClient keyVault = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(this.GetToken), new System.Net.Http.HttpClient());
-            SecretBundle secret = await keyVault.GetSecretAsync(this.BaseUrl, key.Replace("@", string.Empty).Replace(".", string.Empty));
+            SecretBundle secret = await keyVaultClient.GetSecretAsync(BaseUrl, key.Replace("@", string.Empty).Replace(".", string.Empty)).ConfigureAwait(false);
             return secret.Value;
         }
 
         private async Task<string> GetToken(string authority, string resource, string scope)
         {
-            JObject tokenResult = await AuthorizationUtilities.GetADAppToken(authority, resource, this.KeyVaultClientId, this.KeyVaultClientSecret);
+            JObject tokenResult = await AuthorizationUtilities.GetADAppToken(authority, resource, KeyVaultClientId, KeyVaultClientSecret);
             return tokenResult["access_token"].ToString();
         }
     }
