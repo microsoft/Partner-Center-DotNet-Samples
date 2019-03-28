0afd9bb4f779aa06b542bc07b4ea5c771296a8b4
Matt Smith <matsm@microsoft.com>
2019-03-28 16:33:12 -0700

    Whoops I'll need these files

---
+++ sdk/SdkSamples/Subscriptions/CancelSaaSSubscription.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CancelSaaSSubscription.cs" company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Microsoft.Store.PartnerCenter.Samples.Subscriptions
+{
+    /// <summary>
+    /// A scenario that updates an existing customer subscription.
+    /// </summary>
+    public class CancelSaaSSubscription : BasePartnerScenario
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CancelSaaSSubscription"/> class.
+        /// </summary>
+        /// <param name="context">The scenario context.</param>
+        public CancelSaaSSubscription(IScenarioContext context) : base("Cancel existing customer SaaS subscription", context)
+        {
+        }
+
+        /// <summary>
+        /// Executes the scenario.
+        /// </summary>
+        protected override void RunScenario()
+        {
+            var partnerOperations = this.Context.UserPartnerOperations;
+            var customerId = this.ObtainCustomerId();
+            var subscriptionId = this.ObtainSubscriptionId(customerId, "Enter the ID of the subscription to cancel");
+
+            this.Context.ConsoleHelper.StartProgress("Retrieving customer subscription");
+            var existingSubscription = partnerOperations.Customers.ById(customerId).Subscriptions.ById(subscriptionId).Get();
+            this.Context.ConsoleHelper.StopProgress();
+            this.Context.ConsoleHelper.WriteObject(existingSubscription, "Existing subscription");
+
+            this.Context.ConsoleHelper.StartProgress("Cancelling subscription");
+            existingSubscription.Status = Models.Subscriptions.SubscriptionStatus.Deleted;
+            var updatedSubscription = partnerOperations.Customers.ById(customerId).Subscriptions.ById(subscriptionId).Patch(existingSubscription);
+            this.Context.ConsoleHelper.StopProgress();
+
+            this.Context.ConsoleHelper.WriteObject(updatedSubscription, "Cancelled subscription");
+        }
+    }
+}
