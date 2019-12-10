1e879066c560d8c13b7513ed8be54c87eee4c772
Shubham Agarwal <56992243+shuaga@users.noreply.github.com>
2019-12-10 23:50:04 +0530

    Added the Compliance scenarios sample app. (#44)

---
+++ sdk/SdkSamples/Compliance/GetAgreementSignatureStatus.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GetAgreementSignatureStatus.cs" company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Microsoft.Store.PartnerCenter.Samples.Compliance
+{
+    /// <summary>
+    /// A scenario that retrieves the partner's Microsoft Partner Agreement signature status
+    /// </summary>
+    public class GetAgreementSignatureStatus: BasePartnerScenario
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetAgreementSignatureStatus"/> class.
+        /// </summary>
+        /// <param name="context">The scenario context.</param>
+        public GetAgreementSignatureStatus(IScenarioContext context) : base("Get agreement signature status", context)
+        {
+        }
+
+        /// <summary>
+        /// Executes the scenario.
+        /// </summary>
+        protected override void RunScenario()
+        {
+            var partnerOperations = this.Context.UserPartnerOperations;
+
+            this.Context.ConsoleHelper.StartProgress("Retrieving agreement signature status by MPN Id");
+            var agreementSignatureStatusByMpnId = partnerOperations.Compliance.AgreementSignatureStatus.Get(mpnId:"Enter MPN Id");
+            this.Context.ConsoleHelper.StopProgress();
+            this.Context.ConsoleHelper.WriteObject(agreementSignatureStatusByMpnId, "Agreement signature status by MPN Id");
+
+            this.Context.ConsoleHelper.StartProgress("Retrieving agreement signature status by Tenant Id");
+            var agreementSignatureStatusByTenantId = partnerOperations.Compliance.AgreementSignatureStatus.Get(tenantId: "Enter Tenant Id");
+            this.Context.ConsoleHelper.StopProgress();
+            this.Context.ConsoleHelper.WriteObject(agreementSignatureStatusByTenantId, "Agreement signature status by Tenant Id");
+        }
+    }
+}
