785afe66676c0f62631184fd46baa664d319a7a9
cbarneyMS <53239185+cbarneyMS@users.noreply.github.com>
2021-03-25 11:44:17 -0700

    Adding new transition scenarios to the SDK samples (#69)
    * Adding new transition scenarios to the SDK samples

---
+++ sdk/SdkSamples/Subscriptions/GetSubscriptionTransitions.cs
@@ -0,0 +1,39 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GetSubscriptionTransitions.cs" company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Microsoft.Store.PartnerCenter.Samples.Subscriptions
+{
+    /// <summary>
+    /// A scenario that retrieves a customer subscription transitions.
+    /// </summary>
+    public class GetSubscriptionTransitions : BasePartnerScenario
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetSubscriptionTransitions"/> class.
+        /// </summary>
+        /// <param name="context">The scenario context.</param>
+        public GetSubscriptionTransitions(IScenarioContext context) : base("Get customer subscription transitions", context)
+        {
+        }
+
+        /// <summary>
+        /// Executes the scenario.
+        /// </summary>
+        protected override void RunScenario()
+        {
+            var partnerOperations = this.Context.UserPartnerOperations;
+            string customerId = this.ObtainCustomerId("Enter the ID of the customer whom to retrieve their Subscriptions");
+            string subscriptionId = this.ObtainSubscriptionId(customerId, "Enter the ID of the subscription to find transtions for");
+            var subscriptionOperations = partnerOperations.Customers.ById(customerId).Subscriptions.ById(subscriptionId);
+
+            this.Context.ConsoleHelper.StartProgress("Retrieving customer subscription transitions");
+            var transitions = subscriptionOperations.Transitions.Get();
+            this.Context.ConsoleHelper.StopProgress();
+
+            this.Context.ConsoleHelper.WriteObject(transitions, "Customer subscription transitions");
+        }
+    }
+}
