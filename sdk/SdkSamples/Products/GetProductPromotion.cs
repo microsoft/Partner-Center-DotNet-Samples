d425e68b07774309ad7f58e733ed121a20870ca6
tylerbau <tylerbau@microsoft.com>
2021-10-06 12:42:53 -0700

    add product promotions

---
+++ sdk/SdkSamples/Products/GetProductPromotion.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GetProductPromotion.cs" company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Microsoft.Store.PartnerCenter.Samples.Products
+{
+    using System.Globalization;
+
+    /// <summary>
+    /// A scenario that retrieves product promotion details supported in a country.
+    /// </summary>
+    public class GetProductPromotion : BasePartnerScenario
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetProductPromotion"/> class.
+        /// </summary>
+        /// <param name="context">The scenario context.</param>
+        public GetProductPromotion(IScenarioContext context) : base("Get product promotion", context)
+        {
+        }
+
+        /// <summary>
+        /// Executes the scenario.
+        /// </summary>
+        protected override void RunScenario()
+        {
+            var partnerOperations = this.Context.UserPartnerOperations;
+            var productPromotionId = this.ObtainProductId("Enter the ID of the product promotion");
+            string countryCode = this.Context.ConsoleHelper.ReadNonEmptyString("Enter the 2 digit country code of the product", "The country code can't be empty");
+
+            this.Context.ConsoleHelper.StartProgress(string.Format(CultureInfo.InvariantCulture, "Getting details for product promotion {0} in country {1}", productPromotionId, countryCode));
+            var productPromotion = partnerOperations.ProductPromotions.ByCountry(countryCode).ById(productPromotionId).Get();
+            this.Context.ConsoleHelper.StopProgress();
+
+            this.Context.ConsoleHelper.WriteObject(productPromotion, string.Format(CultureInfo.InvariantCulture, "Product promotion details of {0}", productPromotionId));
+        }
+    }
+}
---
+++ sdk/SdkSamples/Products/GetProductPromotions.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GetProductPromotions.cs" company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Microsoft.Store.PartnerCenter.Samples.Products
+{
+    using System.Globalization;
+
+    /// <summary>
+    /// A scenario that retrieves all the product promotions supported in a country and in a segment.
+    /// </summary>
+    public class GetProductPromotions : BasePartnerScenario
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetProductPromotions"/> class.
+        /// </summary>
+        /// <param name="context">The scenario context.</param>
+        public GetProductPromotions(IScenarioContext context) : base("Get product promotions", context)
+        {
+        }
+
+        /// <summary>
+        /// Executes the scenario.
+        /// </summary>
+        protected override void RunScenario()
+        {
+            var partnerOperations = this.Context.UserPartnerOperations;
+            string countryCode = this.Context.ConsoleHelper.ReadNonEmptyString("Enter the 2 digit country code to get its supported products", "The country code can't be empty");
+            string segment = this.Context.ConsoleHelper.ReadNonEmptyString("Enter the segment to get its supported product promotions", "The segment can't be empty");
+
+            this.Context.ConsoleHelper.StartProgress(string.Format(CultureInfo.InvariantCulture, "Getting product promotions in segment {0} in country {1}", segment, countryCode));
+            var productPromotions = partnerOperations.ProductPromotions.ByCountry(countryCode).BySegment(segment).Get();
+            this.Context.ConsoleHelper.StopProgress();
+
+            this.Context.ConsoleHelper.WriteObject(productPromotions, string.Format(CultureInfo.InvariantCulture, "Product promotions in segment {0}", segment));
+        }
+    }
+}
