1bfc05117e13bfca6c79d1b22c5c47b0845c29c1
jonahlytleMSFT <101423586+jonahlytleMSFT@users.noreply.github.com>
2022-03-24 15:11:26 -0700

    Update Program.cs
    weird diff

--- sdk/SdkSamples/Program.cs
+++ sdk/SdkSamples/Program.cs
@@ -1,4 +1,4 @@
-﻿// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
 // <copyright file="Program.cs" company="Microsoft">
 //      Copyright (c) Microsoft Corporation.  All rights reserved.
 // </copyright>
