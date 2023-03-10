9f729b0633c797bf638aecdcaaadb8bf0b3d3d31
Karlen Minasyan <kaminasy@microsoft.com>
2023-03-09 16:02:27 -0800

    Azure entitlement get and cancel operations

---
+++ sdk/SdkSamples/AzureEntitlement/CancelAzureEntitlement.cs
@@ -0,0 +1,57 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CancelAzureEntitlement.cs" company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Microsoft.Store.PartnerCenter.Samples.AzureEntitlement
+{
+    /// <summary>
+    /// Cancel an Azure entitlement.
+    /// </summary>
+    public class CancelAzureEntitlement : BasePartnerScenario
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CancelAzureEntitlement"/> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public CancelAzureEntitlement(IScenarioContext context) : base("Cancel an Azure entitlement", context)
+        {
+        }
+
+        /// <summary>
+        /// Runs the scenario logic. This is delegated to the implementing sub class.
+        /// </summary>
+        protected override void RunScenario()
+        {
+            var partnerOperations = this.Context.UserPartnerOperations;
+
+            var customerId = this.ObtainCustomerId("Enter the ID of the customer whom to retrieve their Subscription");
+            var subscriptionId = this.ObtainSubscriptionId(customerId, "Enter the Azure plan ID to retrieve");
+            var azureEntitlementId = this.ObtainAzureEntitlementId(customerId, subscriptionId, "Enter the Azure entitlement ID to retrieve");
+
+            var selectedCancellationReasonCode = this.Context.ConsoleHelper.ReadNonEmptyString(
+                    "Enter the cancellation reason code (ex: compromise): ",
+                    "Cancellation reason code can't be empty");
+
+            if (selectedCancellationReasonCode != "compromise")
+            {
+                this.Context.ConsoleHelper.Error("Entered cancellation reason code is not supported. Please enter valid cancellation reason code.")
+            }
+
+            var azureEntitlementCancellationRequestContent = new AzureEntitlementCancellationRequestContent
+            {
+                CancellationReason = selectedCancellationReasonCode
+            };
+
+            this.Context.ConsoleHelper.StartProgress("Canceling customer's Azure entitlement");
+            var azureEntitlement = partnerOperations.Customers.ById(customerId)
+                        .Subscriptions.ById(subscriptionId)
+                        .AzureEntitlement.ById(azureEntitlementId)
+                        .Cancel(azureEntitlementCancellationRequestContent);
+
+            this.Context.ConsoleHelper.StopProgress();
+            this.Context.ConsoleHelper.WriteObject(azureEntitlement, "Customer's Azure entitlement");
+        }
+    }
+}
---
+++ sdk/SdkSamples/AzureEntitlement/GetAzureEntitlement.cs
@@ -0,0 +1,43 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GetAzureEntitlement.cs" company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Microsoft.Store.PartnerCenter.Samples.AzureEntitlement
+{
+    /// <summary>
+    /// Get an Azure entitlement.
+    /// </summary>
+    public class GetAzureEntitlement : BasePartnerScenario
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetAzureEntitlement"/> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public GetAzureEntitlement(IScenarioContext context) : base("Get an Azure entitlement", context)
+        {
+        }
+
+        /// <summary>
+        /// Runs the scenario logic. This is delegated to the implementing sub class.
+        /// </summary>
+        protected override void RunScenario()
+        {
+            var partnerOperations = this.Context.UserPartnerOperations;
+
+            var customerId = this.ObtainCustomerId("Enter the ID of the customer whom to retrieve their Subscription");
+            var subscriptionId = this.ObtainSubscriptionId(customerId, "Enter the Azure plan ID to retrieve");
+            var azureEntitlementId = this.ObtainAzureEntitlementId(customerId, subscriptionId, "Enter the Azure entitlement ID to retrieve");
+
+            this.Context.ConsoleHelper.StartProgress("Retrieving customer's Azure entitlement");
+            var azureEntitlement = partnerOperations.Customers.ById(customerId)
+                    .Subscriptions.ById(subscriptionId)
+                    .AzureEntitlement.ById(azureEntitlementId)
+                    .Get();
+
+            this.Context.ConsoleHelper.StopProgress();
+            this.Context.ConsoleHelper.WriteObject(azureEntitlement, "Customer's Azure entitlement");
+        }
+    }
+}
